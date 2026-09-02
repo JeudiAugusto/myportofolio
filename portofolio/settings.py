bc9b528b93a7778f11a1593d84517d8340608acd
Jeudi Augusto Asadullah <jeudiaugustoasadullah@gmail.com>
2026-09-02 19:35:38 +0700

    Rename Django project package

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportofolio.urls'
+ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
@@ -66,7 +66,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportofolio.wsgi.application'
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 
 # Database
