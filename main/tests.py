d551b189f04b9e869a979464a42634c805d6b657
Jeudi Augusto Asadullah <jeudiaugustoasadullah@gmail.com>
2026-09-10 09:09:45 +0700

    test: add Project model and page tests

--- main/tests.py
+++ main/tests.py
@@ -2,49 +2,107 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 class MainTest(TestCase):
     def setUp(self):
         self.experience = Experience.objects.create(
-            title="Cashie - Finance Web App Project",
+            title="Staff Kajian dan Aksi Strategis",
             description=(
-                "Mengembangkan aplikasi web pengelola keuangan "
-                "menggunakan PHP, JavaScript, SQL, CRUD, dan AJAX."
+                "Berperan sebagai Staff Kajian dan Aksi Strategis "
+                "BEM Fasilkom UI 2026."
             ),
-            category="freelance",
+            category="organization",
+        )
+
+        self.project = Project.objects.create(
+            title="Cashie",
+            description=(
+                "Aplikasi web pengelola keuangan yang dikembangkan "
+                "menggunakan PHP dan JavaScript."
+            ),
+            year=2025,
+            role="Team Project",
+            focus="Finance Web App",
+            technologies="PHP, JavaScript, SQL, CRUD, AJAX",
+            repository_url="https://github.com/fzdhl/cashie",
         )
 
     def test_main_url_is_accessible(self):
-        response = self.client.get(reverse("main:show_main"))
+        response = self.client.get(
+            reverse("main:show_main")
+        )
 
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "index.html")
-        self.assertNotContains(response, self.experience.title)
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+        self.assertTemplateUsed(
+            response,
+            "index.html",
+        )
+
+        self.assertContains(
+            response,
+            self.project.title,
+        )
+
+        self.assertNotContains(
+            response,
+            self.experience.title,
+        )
+
+        self.assertContains(
+            response,
+            f'href="{reverse("main:show_projects")}"',
+        )
+
         self.assertContains(
             response,
             f'href="{reverse("main:show_experience")}"',
         )
 
     def test_nonexistent_page_returns_404(self):
-        response = self.client.get("/halaman-yang-tidak-ada/")
+        response = self.client.get(
+            "/halaman-yang-tidak-ada/"
+        )
 
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.status_code,
+            404,
+        )
 
     def test_experience_model(self):
         self.assertEqual(
             str(self.experience),
-            "Cashie - Finance Web App Project",
+            "Staff Kajian dan Aksi Strategis",
         )
-        self.assertEqual(self.experience.category, "freelance")
-        self.assertTrue(self.experience.is_ongoing)
+
+        self.assertEqual(
+            self.experience.category,
+            "organization",
+        )
+
+        self.assertTrue(
+            self.experience.is_ongoing
+        )
 
     def test_experience_page(self):
-        response = self.client.get(reverse("main:show_experience"))
+        response = self.client.get(
+            reverse("main:show_experience")
+        )
 
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "experience.html")
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+        self.assertTemplateUsed(
+            response,
+            "experience.html",
+        )
 
         self.assertContains(
             response,
@@ -58,7 +116,7 @@ class MainTest(TestCase):
 
         self.assertContains(
             response,
-            "Freelance",
+            "Organization",
         )
 
         self.assertContains(
@@ -103,4 +161,113 @@ class MainTest(TestCase):
         self.assertNotContains(
             response,
             "Sedang berlangsung",
+        )
+
+    def test_project_model(self):
+        self.assertEqual(
+            str(self.project),
+            "Cashie",
+        )
+
+        self.assertEqual(
+            self.project.year,
+            2025,
+        )
+
+        self.assertEqual(
+            self.project.role,
+            "Team Project",
+        )
+
+        self.assertEqual(
+            self.project.focus,
+            "Finance Web App",
+        )
+
+        self.assertEqual(
+            self.project.technology_list,
+            [
+                "PHP",
+                "JavaScript",
+                "SQL",
+                "CRUD",
+                "AJAX",
+            ],
+        )
+
+    def test_projects_page(self):
+        response = self.client.get(
+            reverse("main:show_projects")
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+        self.assertTemplateUsed(
+            response,
+            "projects.html",
+        )
+
+        self.assertContains(
+            response,
+            self.project.title,
+        )
+
+        self.assertContains(
+            response,
+            self.project.description,
+        )
+
+        self.assertContains(
+            response,
+            "PHP",
+        )
+
+        self.assertContains(
+            response,
+            "JavaScript",
+        )
+
+        self.assertContains(
+            response,
+            "Team Project",
+        )
+
+        self.assertContains(
+            response,
+            "Finance Web App",
+        )
+
+        self.assertContains(
+            response,
+            self.project.repository_url,
+        )
+
+    def test_empty_projects_page(self):
+        Project.objects.all().delete()
+
+        response = self.client.get(
+            reverse("main:show_projects")
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200,
+        )
+
+        self.assertContains(
+            response,
+            "Belum ada project yang ditambahkan.",
+        )
+
+    def test_projects_link_to_experience(self):
+        response = self.client.get(
+            reverse("main:show_projects")
+        )
+
+        self.assertContains(
+            response,
+            f'href="{reverse("main:show_experience")}"',
         )
